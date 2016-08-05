001ad7e3c25eccd6ccae2e1dd36a55a5f59aa53d
enoch232 <enoch232@gmail.com>
2016-08-05 10:56:26 -0700

    add controller rspec tests for index, new

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe PostsController do
+  let(:post_controller) {PostsController.new}
+  describe "#index" do
+  	it "returns all posts" do
+  	  Post.should_receive(:order).with(:price)
+  	  post_controller.index
+  	end
+  end
+  describe "#new" do 
+  	it "makes a new post" do
+  	  Post.should_receive(:new)
+  	  post_controller.new
+  	end
+  end
+  describe "#copy" do
+  	it "copies a post"
+  end
 
 end
